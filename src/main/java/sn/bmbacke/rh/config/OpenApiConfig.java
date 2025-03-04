4930a3e9820080f7ed1661a6a8c7d4eda6e3fde0
bayembacke221 <mbacke3.mbaye@ucad.edu.sn>
2025-03-04 11:04:45 +0000

    Add OpenAPI configuration and Swagger UI setup

--- src/main/java/sn/bmbacke/rh/config/OpenApiConfig.java
+++ src/main/java/sn/bmbacke/rh/config/OpenApiConfig.java
@@ -1,45 +1,33 @@
 package sn.bmbacke.rh.config;
 
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Contact;
-import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.info.License;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
-import io.swagger.v3.oas.annotations.servers.Server;
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-@OpenAPIDefinition(
-        info = @Info(
-                contact = @Contact(
-                        name = "MalcolmX",
-                        email = "contact@malcol.com",
-                        url = "https://some-url.com"
-                ),
-                description = "OpenApi documentation for Spring Security",
-                title = "OpenApi specification - MalcomX",
-                version = "1.0",
-                license = @License(
-                        name = "Licence name",
-                        url = "https://some-url.com"
-                ),
-                termsOfService = "Terms of service"
-        ),
-        servers = {
-                @Server(
-                        description = "Local ENV",
-                        url = "http://localhost:8088/api/v1"
-                ),
-                @Server(
-                        description = "PROD ENV",
-                        url = "https://some-url.com"
-                )
-        },
-        security = {
-                @SecurityRequirement(
-                        name = "bearerAuth"
-                )
-        }
-)
 
+
+@Configuration
 public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new io.swagger.v3.oas.models.info.Info()
+                        .title("Juricov API")
+                        .version("1.0")
+                        .description("API for Juricov application")
+                        .license(
+                                new io.swagger.v3.oas.models.info.License()
+                                        .name("Apache 2.0")
+                                        .url("https://springdoc.org"))
+                        .contact(new io.swagger.v3.oas.models.info.Contact()
+                                .email("bdk-bank@gmail.com")
+                                .url("https://bdk-bank.com"))
+                        .termsOfService("https://swagger.io/terms/"))
+                .externalDocs(new io.swagger.v3.oas.models.ExternalDocumentation()
+                        .description("Find out more about Juricov")
+                        .url("https://bdk-bank.com"));
+
+    }
 }

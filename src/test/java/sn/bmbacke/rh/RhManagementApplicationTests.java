b4941831d58d2fd402c5a609f5122bb543fbc6bf
bayembacke221 <mbacke3.mbaye@ucad.edu.sn>
2025-03-04 17:01:32 +0000

    Remove empty contextLoads test method from RhManagementApplicationTests

--- src/test/java/sn/bmbacke/rh/RhManagementApplicationTests.java
+++ src/test/java/sn/bmbacke/rh/RhManagementApplicationTests.java
@@ -1,13 +1,10 @@
 package sn.bmbacke.rh;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class RhManagementApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+
 
 }

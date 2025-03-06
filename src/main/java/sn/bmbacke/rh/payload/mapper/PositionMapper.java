99b677ae0eb2770c12f4e48ce2ef321ab1684732
bayembacke221 <mbacke3.mbaye@ucad.edu.sn>
2025-03-06 12:59:12 +0000

    Enhance PositionMapper to support department mapping and DTO conversions

--- src/main/java/sn/bmbacke/rh/payload/mapper/PositionMapper.java
+++ src/main/java/sn/bmbacke/rh/payload/mapper/PositionMapper.java
@@ -2,26 +2,46 @@ package sn.bmbacke.rh.payload.mapper;
 
 
 import org.mapstruct.*;
+import sn.bmbacke.rh.entity.Departement;
+import sn.bmbacke.rh.payload.dto.PositionCreateDTO;
 import sn.bmbacke.rh.payload.dto.PositionDTO;
 import sn.bmbacke.rh.entity.Position;
+import sn.bmbacke.rh.payload.dto.PositionShortDTO;
+import sn.bmbacke.rh.payload.dto.PositionUpdateDTO;
 
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", uses = {DepartementMapper.class})
 public interface PositionMapper {
 
+    @Mapping(target = "department", source = "department")
     PositionDTO toDto(Position position);
 
-    @Mapping(target = "id", ignore = true)
-    @Mapping(target = "createdDate", ignore = true)
-    @Mapping(target = "lastModifiedDate", ignore = true)
-    @Mapping(target = "createdBy", ignore = true)
-    @Mapping(target = "lastModifiedBy", ignore = true)
-    Position toEntity(PositionDTO dto);
+    @Named("toPositionShort")
+    PositionShortDTO toShortDto(Position position);
 
     @Mapping(target = "id", ignore = true)
     @Mapping(target = "createdDate", ignore = true)
     @Mapping(target = "lastModifiedDate", ignore = true)
     @Mapping(target = "createdBy", ignore = true)
     @Mapping(target = "lastModifiedBy", ignore = true)
-    void updateEntityFromDto(PositionDTO dto, @MappingTarget Position position);
+    @Mapping(target = "department", source = "departmentId", qualifiedByName = "positionMapperDepartementFromId")
+    Position toEntity(PositionCreateDTO dto);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "createdDate", ignore = true)
+    @Mapping(target = "lastModifiedDate", ignore = true)
+    @Mapping(target = "createdBy", ignore = true)
+    @Mapping(target = "lastModifiedBy", ignore = true)
+    @Mapping(target = "department", source = "departmentId", qualifiedByName = "positionMapperDepartementFromId")
+    void updateEntityFromDto(PositionUpdateDTO dto, @MappingTarget Position position);
+
+    @Named("positionMapperDepartementFromId")
+    default Departement departementFromId(Long id) {
+        if (id == null) {
+            return null;
+        }
+        Departement departement = new Departement();
+        departement.setId(id);
+        return departement;
+    }
 }

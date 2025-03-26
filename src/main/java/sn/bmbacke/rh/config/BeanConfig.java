55c9f8cf8997ceeb2b44da109a9cd705985d5689
bayembacke221 <mbacke3.mbaye@ucad.edu.sn>
2025-03-26 16:20:17 +0000

    Update CORS configuration to allow requests from Vercel deployment

--- src/main/java/sn/bmbacke/rh/config/BeanConfig.java
+++ src/main/java/sn/bmbacke/rh/config/BeanConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.filter.CorsFilter;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
 @Configuration
 @RequiredArgsConstructor
@@ -54,7 +55,7 @@ public class BeanConfig {
         final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         final CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+        config.setAllowedOrigins(List.of("http://localhost:4200","https://rh-management-ui.vercel.app"));
         config.setAllowedHeaders(Arrays.asList(
                 HttpHeaders.ORIGIN,
                 HttpHeaders.CONTENT_TYPE,
